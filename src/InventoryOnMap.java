2ecd5cfc972bb57e27880376be2b16ac1a89534f
NickolasFarinacci <nickolasfarinacci@gmail.com>
2025-11-20 18:07:53 -0500

    Kernel Implementation

---
+++ src/InventoryOnMap.java
@@ -0,0 +1,122 @@
+import components.map.Map;
+import components.map.Map1L;
+
+/**
+ * An implementation of {@code InventorySecondary} represented as a {@link Map}
+ * with implementations of primary methods.
+ *
+ * @author Nick Farinacci
+ * @param <String>
+ *            type of {@code InventoryKernel} domain (item) entries
+ * @param <Integer>
+ *            type of {@code InventoryKernel} range (associated value) entries
+ * @convention This class uses the standard representation for {@code Map}s
+ *             defined in the components.standard package.
+ * @correspondence this = [value mapping of type String to Integer in a Map]
+ */
+public final class InventoryOnMap extends InventorySecondary {
+
+    /*
+     * Private members --------------------------------------------------------
+     */
+    private Map<String, Integer> map;
+
+    private void createNewRep() {
+        this.map = new Map1L<String, Integer>();
+    }
+
+    /*
+     * Constructors -----------------------------------------------------------
+     */
+    private InventoryOnMap() {
+        this.createNewRep();
+    }
+
+    /*
+     * Standard methods -------------------------------------------------------
+     */
+
+    @SuppressWarnings("unchecked")
+    public InventoryOnMap<String, Integer> newInstance() {
+        try {
+            return this.getClass().getConstructor().newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new AssertionError(
+                    "Cannot construct object of type " + this.getClass());
+        }
+    }
+
+    public void clear() {
+        this.createNewRep();
+    }
+
+    public void transferFrom(Inventory<String, Integer> source) {
+        assert source != null : "Violation of: source is not null";
+        assert source != this : "Violation of: this and source are distinct";
+        assert source instanceof InventoryOnMap<String, Integer> : ""
+                + "Violation of: source is of dynamic type Map1L<?,?>";
+
+        InventoryOnMap<String, Integer> localSource = (InventoryOnMap<String, Integer>) source;
+        this.map = localSource.map;
+        localSource.createNewRep();
+    }
+
+    /*
+     * Kernel methods ---------------------------------------------------------
+     */
+
+    public void addItem(String item, Integer amount) {
+        assert item != null : "Violation of: item is not null";
+        assert amount != null : "Violation of: amount is not null";
+
+        int amountInt = (int) amount;
+
+        if (0 < amountInt && amountInt <= 99) {
+
+            this.map.add(item, amount);
+
+        } else {
+            throw new IllegalArgumentException(
+                    "Violation of: 0 < amount <= 99");
+        }
+
+    }
+
+    Map.Pair<String, Integer> removeItem(String item) {
+        assert item != null : "Violation of: item is not null";
+        assert this.map.hasKey(item) : "Violation of: item is in this";
+
+        return this.map.remove(item);
+
+    }
+
+    boolean hasItem(String item) {
+        assert item != null : "Violation of: item is not null";
+
+        return this.map.hasKey(item);
+
+    }
+
+    int value(String item) {
+        assert item != null : "Violation of: item is not null";
+        assert this.map.hasKey(item) : "Violation of: item is in this";
+
+        return (int) this.map.value(item);
+
+    }
+
+    int totalItems() {
+
+        int total = 0;
+
+        for (Map.Pair<String, Integer> p : this.map) {
+
+            total += (int) p.value();
+
+        }
+
+        return total;
+
+    }
+
+}
